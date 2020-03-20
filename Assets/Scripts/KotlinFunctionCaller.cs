1f4432f0f1544aeb7f4f8984edb0d3821e195b33
NaruAkitsuki <narutoma.montblanc@gmail.com>
2020-03-21 00:49:18 +0900

    KotlinFunctionCaller追加

---
+++ Assets/Scripts/KotlinFunctionCaller.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class KotlinFunctionCaller : MonoBehaviour
+{
+    public void SamplePackageFunction()
+    {
+        using (var javaClass = new AndroidJavaClass("com.redfreshet.samplelibrary.SampleKotlinKt"))
+        {
+            javaClass.CallStatic("SamplePackageFunction");
+        }
+    }
+
+    public void SampleObjectFunction()
+    {
+        using (var javaObj = new AndroidJavaObject("com.redfreshet.samplelibrary.SampleObject"))
+        {
+            javaObj.Call("SampleObjectFunction");
+        }
+    }
+
+    public void SampleJvmCompanionObjectFunction()
+    {
+        using (var javaObj = new AndroidJavaObject("com.redfreshet.samplelibrary.SampleKotlin"))
+        {
+            javaObj.CallStatic("SampleJvmCompanionObjectFunction");
+        }
+    }
+
+    public void SampleClassFunction()
+    {
+        using (var javaObj = new AndroidJavaObject("com.redfreshet.samplelibrary.SampleKotlin"))
+        {
+            javaObj.Call("SampleClassFunction");
+        }
+    }
+}
